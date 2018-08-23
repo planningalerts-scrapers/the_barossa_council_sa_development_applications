01b9bb9b9d853d3577480a2e046e1a091f7e01a0
MichaelBone <Michael.Bone@senet.com.au>
2018-08-23 23:13:34 +0930

    Removed the unused on_notice_from and on_notice_to columns.

--- scraper.ts
+++ scraper.ts
@@ -26,8 +26,12 @@ async function initializeDatabase() {
     return new Promise((resolve, reject) => {
         let database = new sqlite3.Database("data.sqlite");
         database.serialize(() => {
-            database.run("create table if not exists [data] ([council_reference] text primary key, [address] text, [description] text, [info_url] text, [comment_url] text, [date_scraped] text, [date_received] text, [on_notice_from] text, [on_notice_to] text)");
-            resolve(database);
+            database.all("PRAGMA table_info('data')", (error, rows) => {
+                if (rows.some(row => row.name === "on_notice_from"))
+                    database.run("drop table [data]");  // ensure that the on_notice_from (and on_notice_to) columns are removed
+                database.run("create table if not exists [data] ([council_reference] text primary key, [address] text, [description] text, [info_url] text, [comment_url] text, [date_scraped] text, [date_received] text)");
+                resolve(database);
+            });
         });
     });
 }
@@ -36,7 +40,7 @@ async function initializeDatabase() {
 
 async function insertRow(database, developmentApplication) {
     return new Promise((resolve, reject) => {
-        let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        let sqlStatement = database.prepare("insert or ignore into [data] values (?, ?, ?, ?, ?, ?, ?)");
         sqlStatement.run([
             developmentApplication.applicationNumber,
             developmentApplication.address,
@@ -44,9 +48,7 @@ async function insertRow(database, developmentApplication) {
             developmentApplication.informationUrl,
             developmentApplication.commentUrl,
             developmentApplication.scrapeDate,
-            developmentApplication.receivedDate,
-            null,
-            null
+            developmentApplication.receivedDate
         ], function(error, row) {
             if (error) {
                 console.error(error);
