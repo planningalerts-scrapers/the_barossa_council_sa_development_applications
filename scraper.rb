1f46a64d3e8d163e3e0187e3afee75ea1b2f12bb
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 04:11:15 +1000

    Remove whitespace modifications to address

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,6 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape(
+EpathwayScraper.scrape_and_save(
   "https://epayments.barossa.sa.gov.au/ePathway/Production",
   list_type: :last_30_days, force_detail: true
-) do |record|
-  record["address"] = record["address"].squeeze(" ")
-
-  EpathwayScraper.save(record)
-end
+)
