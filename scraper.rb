a2026f721cced17e83c4b5475188c2b92883874f
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 02:25:15 +1000

    Always scrape the detail page to force full addresses

--- scraper.rb
+++ scraper.rb
@@ -2,7 +2,7 @@ require "epathway_scraper"
 
 EpathwayScraper.scrape(
   "https://epayments.barossa.sa.gov.au/ePathway/Production",
-  list_type: :last_30_days
+  list_type: :last_30_days, force_detail: true
 ) do |record|
   record["address"] = record["address"].squeeze(" ")
 
