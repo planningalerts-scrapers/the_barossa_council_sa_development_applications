f34cbad2edd94ed4b406600d12f821d6c5288921
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 12:08:16 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,5 @@ require "epathway_scraper"
 
 EpathwayScraper.scrape_and_save(
   "https://epayments.barossa.sa.gov.au/ePathway/Production",
-  list_type: :last_30_days, force_detail: true
+  list_type: :last_30_days, force_detail: true, state: "SA"
 )
